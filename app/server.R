d718e5f8937d8f08603a7eb2c9f0de41bd59e2d5
C. Witt <c.wittekind@posteo.de>
2024-07-02 16:27:17 +0200

    clarified observed events, comments and visibility

--- app/server.R
+++ app/server.R
@@ -3,27 +3,17 @@ server <- function(input, output, session) {
 
   required_files <- c("../data/pareto_genomes.txt", "../data/pareto_fitness.txt", "../data/hru.con", "../data/measure_location.csv")   # Example file names
   
-  # Check if required files exist
-  checkFiles <- reactive({
-    sapply(required_files, function(file) file.exists(file))
-  })
+  # Check if required files exist #####
+  checkFiles <- reactive({sapply(required_files, function(file) file.exists(file))})
   
   # Output message about file status
+   output$fileStatusMessage <- renderText({if (all(checkFiles())) {""} else {paste("The following file(s) are missing:", paste(required_files[!checkFiles()], collapse = ", "))}})
   
-  output$fileStatusMessage <- renderText({
-    if (all(checkFiles())) {
-      ""
-    } else {
-      paste("The following file(s) are missing:", paste(required_files[!checkFiles()], collapse = ", "))
-    }
-  })
-  
-  # write a new config.ini with selected variables and find the highest correlation
+   
   observeEvent(input$run,{
+    # write a new config.ini with selected variables and find the highest correlation
     write_corr(vars = input$selements)
-  })
-  
-  observeEvent(input$run, {
+    
     # Define the command to run the Python script
     py_script <- "../python_files/correlation_matrix.py"
     cmd <- paste("python", py_script)
@@ -32,43 +22,24 @@ server <- function(input, output, session) {
     result <- system(cmd, intern = TRUE)
     
     
-    
-    # Read the CSV file
+    # Correlation Plot (does not change for different thresholds)
     csv_file <- "../output/correlation_matrix.csv"
-    
-    validate(
-      need(file.exists(csv_file), "CSV file not found.")
-    )
-    
-  
+    validate(need(file.exists(csv_file), "CSV file not found."))
     corr = read.csv(csv_file, row.names = 1)
-    
-    # Display the CSV content in the Shiny app
-    output$corrplot <- renderPlot({
-      plt_corr(corr)
-    })
-    
+    output$corrplot <- renderPlot({plt_corr(corr)})
+ 
+  # events tied to a change in threshold, however also tied to change in selected variables, therefore also observe run
+  observeEvent(input$thresh,{
     # Highest correlation under selected threshold
-    output$corrtable <- renderTable({
-     
-      find_high_corr(corr,threshold=input$thresh, tab=T) #tab = T means this returns the full table, =F is for pulling variables
-    })
-    
-    output$selements <- renderTable({
-      input$selements
-      },rownames = T,colnames = F)
+    output$corrtable <- renderTable({find_high_corr(corr,threshold=input$thresh, tab=T)}) #tab = T means this returns the full table, =F is for pulling variables
+   
+    # Top Table with selected elements
+    output$selements <- renderTable({input$selements},rownames = T,colnames = F)
     })
   
-   output$excla <- renderText({
-    input$excl
+    # Drop Down Menu with subset of those with selected threshold (outsider for now so it doesn't delete values when playing with threshold)
+    observe({ updateSelectInput(session, "excl",choices = find_high_corr(corr,threshold=input$thresh, tab=F))})
   })
- 
-   
-   observe({
-     updateSelectInput(session, "excl",
-                       choices = find_high_corr(corr,threshold=input$thresh, tab=F)
-                       )
-   })
 }
 
 
