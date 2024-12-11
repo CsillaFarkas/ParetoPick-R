107ddd3114124bcdee9ccc3bb89ce6b154a66218
C. Witt <c.wittekind@posteo.de>
2024-12-11 15:11:35 +0100

    double lib load removed

--- app/global.R
+++ app/global.R
@@ -24,42 +24,9 @@ foo1(c("configr","corrplot","cowplot", "dplyr","DT", "fs","fst","geosphere", "gg
        "shinyFiles", "shinyjs","shinythemes",  "shinyWidgets",  "sp", 
        "spdep",  "tibble",  "tidyr",  "tidyverse",  "tmap",  "viridis","webshot"))
 
-## libraries required for app (not convert_optain)
-library(configr)#
-library(corrplot)#
-library(dplyr)#
-library(DT)#
-library(fs)
-library(fst)
-library(ggplot2)
-library(gridExtra) # or patchwork
-library(htmltools)
-library(htmlwidgets) #save html files
-library(ini)#
-library(leaflet)#
-library(processx) # manage system processes from within shiny (e.g. for pulling R output into shiny)
-library(purrr)
-library(leafsync)
-library(mapview)
-library(quanteda)
-library(RColorBrewer)
-# library(reactlog)
-library(reticulate)
-library(scales)
-library(sf)#
-library(shiny)#
+
 options(shiny.maxRequestSize = 1000*1024^2)
-library(shinycssloaders) # for withSpinner()
-library(shinydashboard)#
-library(shinyFiles) #drag and drop
-library(shinyjs) # for hiding parts of the app
-library(shinyWidgets)#
-library(tibble)
-library(tidyr)
-library(tidyverse)
-library(viridis)
-library(webshot)
-# reactlog_enable()
+
 options(warn = -1)
 source("functions.R")
 
